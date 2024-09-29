2dcf6850c87be9b62f539badaebaaea925363a64
fangtiancheng <1773701277@qq.com>
2024-09-29 13:11:58 +0800

    update

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from utils.responseImage_beta import ResponseImage
 from utils.sjtuClassroomApi import getRoomCourse
-from typing import Optional
+from typing import Optional, List, Dict, Any, Tuple
 import datetime
 import json
+
+def processJsInfo(jsInfo: Dict[str, List[Dict[str, Any]]]) -> List[Tuple[str, List[bool]]]:
+    result:List[Tuple[str, List[bool]]] = []
+    for floor in jsInfo.get('floorList', []):
+        for room in floor.get('children', {}):
+            roomName:str = room.get('name', 'unknow')
+            roomSecs = [False] * 15 # 是否有课
+            for course in room.get('roomCourseList', []):
+                startSec:int = course.get('startSection', 0)
+                endSec:int = course.get('endSection', 0)
+            for sec in range(startSec, endSec + 1):
+                roomSecs[sec] = True # TODO: IndexError
+            result.append((roomName, roomSecs))
+    return result
 
 def drawJs(jsInfo, savePath:str) -> Optional[str]:
     # TODO:
@@ -12,6 +26,8 @@ if __name__ == '__main__':
     targetBuilding = '东下院'
     jsInfoSavePath = targetBuilding + '.json'
     jsInfo = getRoomCourse(targetBuilding, datetime.date.today())
-    with open(jsInfoSavePath, 'w') as f:
-        json.dump(jsInfo, f, ensure_ascii=False, indent=2)
-    print('save to:', jsInfoSavePath)
+    # with open(jsInfoSavePath, 'w') as f:
+    #     json.dump(jsInfo, f, ensure_ascii=False, indent=2)
+    # print('save to:', jsInfoSavePath)
+    jsInfo = processJsInfo(jsInfo)
+    print(jsInfo)
